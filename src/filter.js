f70ea1cc0ccaa5e545f0d826472e152576261813
GokulnathP <4gokul2000@gmail.com>
2021-01-07 20:17:11 +0530

    Add. filter functionlity with respect to passed callback function

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,22 @@
+const head = require("./head");
+const tail = require("./tail");
+
+const filterEveryElement = (inputList, outputList, callbackFunction) => {
+    if(inputList?.length > 0) {
+        const firstElement = head(inputList);
+        const canIAddToOutput = callbackFunction(firstElement);
+
+        if(canIAddToOutput) {
+            const outputListLength = outputList.length;
+            outputList[outputListLength] = firstElement;
+        }
+        return filterEveryElement(tail(inputList), outputList, callbackFunction);
+    }
+    return outputList;
+}
+
 const filter = (list, callbackFunction) => {
-    return list;
+    return filterEveryElement(list, [], callbackFunction);
 }
 
 module.exports = filter;
--- src/filter.spec.js
+++ src/filter.spec.js
@@ -18,4 +18,27 @@ describe("Filter", () => {
         expect(filteredOutput).toEqual([1,2,3]);
     });
 
+    it("filter([1,2,3],x => false) should give []", () => {
+        const callbackFunction = value => false;
+
+        const filteredOutput = filter([1,2,3], callbackFunction);
+
+        expect(filteredOutput).toEqual([]);
+    });
+
+    it("filter([1,2,3],x => x > 1) should give [2,3]", () => {
+        const callbackFunction = value => value > 1;
+
+        const filteredOutput =  filter([1,2,3], callbackFunction);
+
+        expect(filteredOutput).toEqual([2,3]);
+    });
+
+    it("filter(['a','B','c','D'], filterUpperCase) should be [B,D]", () => {
+        const filterUpperCase = character => character.toUpperCase() === character;
+
+        const filteredOutput = filter(['a', 'B', 'c', 'D'], filterUpperCase);
+
+        expect(filteredOutput).toEqual(['B', 'D']);
+    });
 });
